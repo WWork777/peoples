73de9b3788e3e6d6773a502b9b04d8354bc7c230
dd <xeon3883@gmail.com>
2026-01-21 13:27:13 +0300

    reviews upd

--- src/components/home-page/reviews/reviews.jsx
+++ src/components/home-page/reviews/reviews.jsx
@@ -5,9 +5,12 @@ export default function Reviews() {
 		<section id='reviews' className='reviews bg-(--accent-color)'>
 			<div className='container max-w-7xl mx-auto px-3.75 pb-10'>
 				<div className='py-10 '>
-					<h2 className='text-white text-[25px] sm:text-[32px] font-bold text-left'>
-						ОТЗЫВЫ НАШИХ УЮТНЫХ ГОСТЕЙ
+					<h2 className='text-white text-[25px] sm:text-[32px] font-bold text-center uppercase pb-5'>
+						Самый высокий рейтинг
 					</h2>
+					<h3 className='text-white font-medium text-center uppercase'>
+						Отличная репутация на всех площадках Яндекс, Avito, 2GIS
+					</h3>
 				</div>
 				<div>
 					<ReviewsCarousel />
