5e19b81a3ea6da5dfb54aebc603484a87f3136ad
id0ntc4re <parsikovevgenij470@gmail.com>
2026-01-24 23:43:49 +0700

    qwe

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,36 +1,36 @@
-import { Montserrat } from 'next/font/google'
-import './globals.css'
+import { Montserrat } from "next/font/google";
+import "./globals.css";
 
 const montserrat = Montserrat({
-	variable: '--font-montserrat',
-	subsets: ['cyrillic'],
-})
+  variable: "--font-montserrat",
+  subsets: ["cyrillic"],
+});
 
 export const metadata = {
-	title: 'Люди в уюте',
-	description: 'Ваш лучший отдых с видом на заснеженные вершины гор',
-	icons: {
-		icon: [
-			{ rel: 'icon', type: 'image/svg+xml', url: '/favicon/favicon.svg' },
-			{
-				rel: 'icon',
-				type: 'image/png',
-				sizes: '96x96',
-				url: '/favicon/favicon-96x96.png',
-			},
-		],
-		shortcut: '/favicon/favicon.ico',
-		apple: '/favicon/apple-touch-icon.png',
-	},
-	manifest: '/favicon/site.webmanifest',
-}
+  title: "Люди в уюте",
+  description: "Ваш лучший отдых с видом на заснеженные вершины гор",
+  icons: {
+    icon: [
+      { rel: "icon", type: "image/svg+xml", url: "/favicon/favicon.svg" },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "96x96",
+        url: "/favicon/favicon-96x96.png",
+      },
+    ],
+    shortcut: "/favicon/favicon.ico",
+    apple: "/favicon/apple-touch-icon.png",
+  },
+  manifest: "/favicon/site.webmanifest",
+};
 
 export default function RootLayout({ children }) {
-	return (
-		<html lang='ru'>
-			<body className={`${montserrat.variable} antialiased`}>
-				<div className='max-w-480 mx-auto'>{children}</div>
-			</body>
-		</html>
-	)
+  return (
+    <html lang="ru">
+      <body className={`${montserrat.variable} antialiased`}>
+        <div className="max-w-480 mx-auto">{children}</div>
+      </body>
+    </html>
+  );
 }
