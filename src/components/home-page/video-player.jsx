9de1565f3fe4335a529bb062cb8ce1af2591411b
Ser0jka <sersur2007@gmail.com>
2026-03-31 19:46:33 +0700

    upd

--- src/components/home-page/video-player.jsx
+++ src/components/home-page/video-player.jsx
@@ -1,21 +1,51 @@
-import Link from 'next/link'
-export default function VideoPlayer() {
-    return(
-        <section className='camera-feed py-10'>
-			<div className='container max-w-7xl mx-auto px-3.75'>
-				<h2 className='text-(--accent-color) text-[25px] sm:text-[32px] font-bold text-center leading-8 xs:leading-normal pb-5'>
-					Онлайн камера с территории комплекса
-				</h2>
-				<div className='w-full max-w-3xl mx-auto overflow-hidden rounded-lg shadow-lg aspect-video bg-black'>
-					<iframe
-						src='http://localhost:1984/webrtc.html?src=camera1&media=video'
-						className='w-full h-full border-0'
-						allow='fullscreen'
-						title='Онлайн камера с территории комплекса'
-					/>
-				</div>
-			</div>
-		</section>
-    );
+'use client';
+import { useEffect, useRef } from 'react';
+import Hls from 'hls.js';
 
+export default function VideoPlayer() {
+    const videoRef = useRef(null);
+    // Путь к файлу, который генерирует ваш бэкенд/ffmpeg
+    const videoSrc = '/live/stream.m3u8'; 
+
+    useEffect(() => {
+        const video = videoRef.current;
+        if (!video) return;
+
+        if (video.canPlayType('application/vnd.apple.mpegurl')) {
+            // Для Safari
+            video.src = videoSrc;
+        } else if (Hls.isSupported()) {
+            // Для всех остальных (Chrome, Firefox и т.д.)
+            const hls = new Hls({
+                enableWorker: true,
+				lowLatencyMode: true,
+				maxBufferLength: 10, 
+				maxMaxBufferLength: 30,
+				liveSyncDuration: 3,
+            });
+            hls.loadSource(videoSrc);
+            hls.attachMedia(video);
+        }
+    }, []);
+
+	return (
+        <section className='camera-feed'>
+            <div className='container max-w-360 mx-auto px-3.75'>
+                <h2 className='text-(--accent-color) text-[25px] sm:text-[32px] font-bold text-center'>
+					ОНЛАЙН КАМЕРА С ТЕРРИТОРИИ КОМПЛЕКСА
+				</h2>
+                <div className='py-10'>
+                    <video 
+						ref={videoRef}
+                        autoPlay 
+                        muted 
+                        playsInline 
+                        controls
+                        style={{ width: '100%', height: 'auto', borderRadius: '24px' }}
+                    >
+                    </video>
+                </div>
+            </div>
+        </section>
+    );
 }
