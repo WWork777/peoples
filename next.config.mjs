b4d077a19934fcadd46a53f8e366978ba0c77bf8
id0ntc4re <parsikovevgenij470@gmail.com>
2026-04-21 14:02:33 +0700

    qwe

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Статика (JS/CSS) теперь полетит через CDN
-  assetPrefix: "https://cdn.xn--b1adekn9bg8fe.xn--p1ai",
-
-  images: {
-    loader: "custom",
-    loaderFile: "./loader.js",
-    formats: ["image/avif", "image/webp"],
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "cdn.xn--b1adekn9bg8fe.xn--p1ai",
-      },
-    ],
-  },
-  // Твои остальные настройки (headers, redirects и т.д.) без изменений
+  /* config options here */
 };
 
 export default nextConfig;
